80707ecd8fb272774b1ced55f5ed6cca70e19fac
Momin Mohsinali Sokatali <24bca088@charusat.edu.in>
2026-08-15 16:18:44 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Story from './components/Story.jsx'
+import Countdown from './components/Countdown.jsx'
+import Events from './components/Events.jsx'
+import Venue from './components/Venue.jsx'
+import Gallery from './components/Gallery.jsx'
+import DressCode from './components/DressCode.jsx'
+import Travel from './components/Travel.jsx'
+import Footer from './components/Footer.jsx'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Story />
+      <Countdown />
+      <Events />
+      <Venue />
+      <Gallery />
+      <DressCode />
+      <Travel />
+      <Footer />
+    </>
+  )
+}
